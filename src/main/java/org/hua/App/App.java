ef6b8ca18e3a7370de0e5eb3342516d7d9963d15
Athanasioschourlias <tha.noschourlias@yahoo.com>
2020-12-04 13:25:15 +0200

    Write method and some documentation

--- src/main/java/org/hua/App/App.java
+++ src/main/java/org/hua/App/App.java
@@ -14,20 +14,20 @@ public class App{
         Frequencies r3 = new Frequencies("input3.txt");
 
 
-        
+
         //calling the exit method at the end of the app to close the file.
         //TODO:DELETE THE FILE WE CREATED FOR THE FREQUENCIES
-        Path path = Paths.get("./frequencies.dat");
-        if(!Files.exists(path) && Files.isReadable(path)){
-            System.out.println("Plese check if the given path is right or you have the right permissions to this file.");
-            throw new FileNotFoundException();
-        }
-        File myObj = new File("frequencies.dat");
-        if (myObj.delete()) {
-            System.out.println("Deleted the file: " + myObj.getName());
-        } else {
-            System.out.println("Failed to delete the file.");
-        }
+//        Path path = Paths.get("./frequencies.dat");
+//        if(!Files.exists(path) && Files.isReadable(path)){
+//            System.out.println("Plese check if the given path is right or you have the right permissions to this file.");
+//            throw new FileNotFoundException();
+//        }
+//        File myObj = new File("frequencies.dat");
+//        if (myObj.delete()) {
+//            System.out.println("Deleted the file: " + myObj.getName());
+//        } else {
+//            System.out.println("Failed to delete the file.");
+//        }
 
     }
 }
