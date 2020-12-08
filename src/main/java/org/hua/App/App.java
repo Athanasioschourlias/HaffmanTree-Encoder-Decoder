f5af90aafd521c47d154d5614f9a7ab0a147081b
Athanasioschourlias <tha.noschourlias@yahoo.com>
2020-12-08 22:27:32 +0200

    update to much the main
    Signed-off-by: Athanasioschourlias <tha.noschourlias@yahoo.com>

--- src/main/java/org/hua/App/App.java
+++ src/main/java/org/hua/App/App.java
@@ -1,10 +1,8 @@
 package org.hua.App;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import org.hua.App.Huffman_tree.Node;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class App{
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -12,65 +10,11 @@ public class App{
         Frequencies r2 = new Frequencies("input2.txt");
         Frequencies r3 = new Frequencies("input3.txt");
 
-
+        for(int i = 0; i < 128; i++)
+        {
+            r.count[i]=r.count[i]+r2.count[i]+r3.count[i];
+        }
         r.writeFile();
-        r2.writeFile();
-        r3.writeFile();
-
-
-        /**
-         *"DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * v
-         * v"DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * v
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         * "DEN EISAI FLOGERAS AN TO VLEPEIS"
-         *
-         */
-
-        //print test of the file.and then deleting it
-        final File InFile;
-        int nextChar;
-        char ch;
-        int[] count = new int[128];
-        InFile = new File("frequencies.dat");
-        BufferedReader reader = new BufferedReader(new FileReader(InFile));
-
-        for(int j = 0;j < 3; j++) {
-            for (int i = 0; i < 128; i++) {
-                System.out.printf("%c %s", i, reader.readLine());
-                System.out.println("");
-
-            }
-        }
-        
         //calling the exit method at the end of the app to close the file.
-        //TODO:DELETE THE FILE WE CREATED FOR THE FREQUENCIES
-        Path path = Paths.get("./frequencies.dat");
-        if(!Files.exists(path) && Files.isReadable(path)){
-            System.out.println("Plese check if the given path is right or you have the right permissions to this file.");
-            throw new FileNotFoundException();
-        }
-        File myObj = new File("frequencies.dat");
-        if (myObj.delete()) {
-            System.out.println("Deleted the file: " + myObj.getName());
-        } else {
-            System.out.println("Failed to delete the file.");
-        }
-        String test = "abccccccccccdeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeefg";
-
-        Huffman_tree huffman = new Huffman_tree();
-        int[] ft = huffman.FrequencyTable(test);
-        Node n= huffman.Huffman(ft);
-        System.out.println(n);
-        
-
     }
 }
