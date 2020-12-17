3ac054b0e8246426be3e2768013c6dfe303c8de8
Thanos chourlias <thanos.chourlias@gmail.com>
2020-12-17 12:43:11 +0200

    Documenting the huffman interface.
    Signed-off-by: Athanasioschourlias <tha.noschourlias@yahoo.com>

--- src/main/java/org/hua/App/HuffmanTreeI.java
+++ src/main/java/org/hua/App/HuffmanTreeI.java
@@ -1,10 +1,17 @@
 package org.hua.App;
 
 public interface HuffmanTreeI<Node> {
-    
-    //creating the structure of the tree 
+
+    /**
+     * creating the structure of the tree
+     * @return Node
+     */
     Node Huffman();
-    //reads fequencies from our file in an array so they can be inserted in the min heap
+
+    /**
+     * reads fequencies from our file in an array so they can be inserted in the min heap
+     * @return int table
+     */
     int[] ReadFrequencies();
 
 
