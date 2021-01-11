b472f21eae4d010865ceee045c0a098ff18a6047
liapisn <nikos@liapis.info>
2021-01-11 23:25:02 +0200

    TELOS με το γαμημενο το 4ο παρτ!!!!

--- src/main/java/org/hua/App/EncodingFile.java
+++ src/main/java/org/hua/App/EncodingFile.java
@@ -2,6 +2,7 @@ package org.hua.App;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.ArrayDeque;
 import java.util.BitSet;
 
 
@@ -9,7 +10,9 @@ public class EncodingFile {
 
     private int nextChar;
     private String[] cdmap;
-    private static int counter =0;
+    private static int counterbit =0;
+    private static int counterByte=0;
+    ArrayDeque<Integer> stuck = new ArrayDeque<>();
 
     public EncodingFile(){
         this.nextChar = 0;
@@ -20,56 +23,91 @@ public class EncodingFile {
 
         //reading the huffman encoding values for each letter from the codes.dat file
         cdmap = readCodes(codings);
+        BitSet buffer = new BitSet(8);
+        byte[] b;
         try (BufferedReader reader = new BufferedReader(new FileReader (inputFile)))
         {
-
             FileOutputStream out = new FileOutputStream(outputFile);;
             do
             {
                 nextChar = reader.read();
                 //Reading the ascii chars
                 if (nextChar >= 1 && nextChar < 256){
-
-                    try {
-                        out.write(setingBits(cdmap[nextChar]).toByteArray());
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    for(int i=0;i< cdmap[nextChar].length();i++)
+                    {
+                        if(cdmap[nextChar].charAt(i)=='1')
+                        {
+                            if(stuck.size()==8)
+                            {
+                                for (int j=0;j<8;j++)
+                                {
+                                    if(stuck.getFirst()==1)
+                                    {
+                                        buffer.set(j);
+                                    }
+                                    stuck.removeFirst();
+                                }
+                                b=buffer.toByteArray();
+                                out.write(b);
+                                counterByte++;
+                                buffer.clear();
+                                stuck.clear();
+                            }
+                            stuck.add(1);
+                            counterbit++;
+                        }
+                        else
+                        {
+                            if(stuck.size()==8)
+                            {
+                                for (int j=0;j<8;j++)
+                                {
+                                    if(stuck.getFirst()==1)
+                                    {
+                                        buffer.set(j);
+                                    }
+                                    stuck.removeFirst();
+                                }
+                                b=buffer.toByteArray();
+                                out.write(b);
+                                counterByte++;
+                                buffer.clear();
+                                stuck.clear();
+                            }
+                            stuck.add(0);
+                            counterbit++;
+                        }
                     }
                 }
 
             }while (nextChar != -1);
 
-            byte[] bytes = ByteBuffer.allocate(4).putInt(counter).array();
-
-            out.write(bytes,0,4);
+            byte[] bytes = ByteBuffer.allocate(4).putInt(counterbit).array();
+            if(!stuck.isEmpty())
+            {
+                for (int j=0;j<stuck.size()-1;j++)
+                {
+                    if(stuck.getFirst()==1)
+                    {
+                        buffer.set(j);
+                    }
+                    stuck.removeFirst();
+                }
+                b=buffer.toByteArray();
+                out.write(b);
+                counterByte++;
+                buffer.clear();
+                stuck.clear();
+            }
+            byte[] eofByte="EOF".getBytes();
+            out.write(eofByte,0,3);
             out.close();
 
         } catch (IOException x)
         {
             System.err.format("IOException: %s%n", x);
         }
-
         System.out.println("Done");
-
-    }
-
-    private static BitSet setingBits(String cdmap){
-        BitSet buffer = new BitSet(cdmap.length());
-        for(int i=0; i < cdmap.length(); i++){
-
-            if (cdmap.charAt(i) == '1')
-            {
-                buffer.set(i, true);
-                counter++;
-            }else
-            {
-                buffer.set(i, false);
-                counter++;
-            }
-        }
-
-        return buffer;
     }
 
     private static String[] readCodes(File codings){
@@ -87,15 +125,12 @@ public class EncodingFile {
                 answer = nextline.split(" ");
 
                 cdmap[i]= answer[1];
-//                System.out.println(answer[1]);
                 //Reading the ascii chars
 
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return cdmap;
-
     }
 }
